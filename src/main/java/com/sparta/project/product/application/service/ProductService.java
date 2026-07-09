7da626530fcac9f1a02b63fbb39d3d98435a4a7d
kuyhxl <ozzing727@naver.com>
2026-07-09 09:58:21 +0900

    [Feat] 상품 등록 서비스 로직 구현

--- src/main/java/com/sparta/project/product/application/service/ProductService.java
+++ src/main/java/com/sparta/project/product/application/service/ProductService.java
@@ -1,4 +1,33 @@
 package com.sparta.project.product.application.service;
 
+import com.sparta.project.product.domain.entity.Product;
+import com.sparta.project.product.domain.repository.ProductRepository;
+import com.sparta.project.product.presentation.dto.request.ProductCreateRequest;
+import com.sparta.project.product.presentation.dto.response.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    // 상품 등록
+    @Transactional
+    public ProductResponse createProduct(ProductCreateRequest request){
+        // 1. 요청 DTO -> Entity 생성
+        Product product = Product.create(
+                request.getStoreId(),
+                request.getName(),
+                request.getPrice(),
+                request.getDescription(),
+                request.getDisplayOrder()
+        );
+        // 2. Save
+        Product savedProduct = productRepository.save(product);
+        // 3. Entity -> Response DTO 변환 후 반환
+        return ProductResponse.from(savedProduct);
+    }
 }
