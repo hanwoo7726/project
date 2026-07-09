3a5d8d2e8de873f89c46f056736e6c4077090793
kuyhxl <ozzing727@naver.com>
2026-07-09 11:25:54 +0900

    [Feat] 상품 등록 API 컨트롤러 구현

--- src/main/java/com/sparta/project/product/presentation/controller/ProductController.java
+++ src/main/java/com/sparta/project/product/presentation/controller/ProductController.java
@@ -1,9 +1,29 @@
 package com.sparta.project.product.presentation.controller;
 
+import com.sparta.project.product.application.service.ProductService;
+import com.sparta.project.product.presentation.dto.request.ProductCreateRequest;
+import com.sparta.project.product.presentation.dto.response.ProductResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+
+    // 상품 등록
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(
+            @Valid @RequestBody ProductCreateRequest request
+    ){
+        ProductResponse response = productService.createProduct(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
