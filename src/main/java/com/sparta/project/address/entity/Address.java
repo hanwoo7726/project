d631ecebaa4a03f7dd974f33fbbbd264bbf4ba65
kanghm199 <kanghm199@gmail.com>
2026-07-14 14:42:35 +0900

    fix: 주소, 장바구니, 찜 프로젝트 구조 수정

---
+++ src/main/java/com/sparta/project/address/entity/Address.java
@@ -0,0 +1,118 @@
+package com.sparta.project.address.entity;
+
+import com.sparta.project.address.dto.request.ReqCreateAddressDto;
+import com.sparta.project.address.dto.request.ReqUpdateAddressDto;
+import com.sparta.project.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Table(name = "p_address")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "address_name", nullable = false, length = 100)
+  private String addressName;
+
+  @Column(name = "postal_code", nullable = false, length = 5)
+  private String postalCode;
+
+  @Column(name = "address", nullable = false)
+  private String address;
+
+  @Column(name = "detail_address", nullable = false)
+  private String detailAddress;
+
+  @Column(name = "delivery_request")
+  private String deliveryRequest;
+
+  @Column(name = "entrance_password")
+  private String entrancePassword;
+
+  @Column(name = "is_default")
+  private boolean isDefault;
+
+  private Address(
+      User user,
+      String addressName,
+      String postalCode,
+      String address,
+      String detailAddress,
+      String deliveryRequest,
+      String entrancePassword,
+      boolean isDefault
+  ) {
+    this.user = user;
+    this.addressName = addressName;
+    this.postalCode = postalCode;
+    this.address = address;
+    this.detailAddress = detailAddress;
+    this.deliveryRequest = deliveryRequest;
+    this.entrancePassword = entrancePassword;
+    this.isDefault = isDefault;
+  }
+
+  public static Address create(
+      User user,
+      ReqCreateAddressDto dto,
+      boolean isDefault
+  ) {
+    return new Address(
+        user,
+        dto.getAddressName(),
+        dto.getPostalCode(),
+        dto.getAddress(),
+        dto.getDetailAddress(),
+        dto.getDeliveryRequest(),
+        dto.getEntrancePassword(),
+        isDefault
+    );
+  }
+
+  public void updateAddress(
+      ReqUpdateAddressDto reqDto
+  ) {
+
+    if (reqDto.getAddressName() != null) {
+      this.addressName = reqDto.getAddressName();
+    }
+
+    if (reqDto.getPostalCode() != null) {
+      this.postalCode = reqDto.getPostalCode();
+    }
+
+    if (reqDto.getAddress() != null) {
+      this.address = reqDto.getAddress();
+    }
+
+    if (reqDto.getDetailAddress() != null) {
+      this.detailAddress = reqDto.getDetailAddress();
+    }
+
+    if (reqDto.getDeliveryRequest() != null) {
+      this.deliveryRequest = reqDto.getDeliveryRequest();
+    }
+
+    if (reqDto.getEntrancePassword() != null) {
+      this.entrancePassword = reqDto.getEntrancePassword();
+    }
+
+  }
+
+  public void setDefaultAddress() {
+    this.isDefault = true;
+  }
+}
---
+++ src/main/java/com/sparta/project/cart/controller/CartController.java
@@ -0,0 +1,107 @@
+package com.sparta.project.cart.controller;
+
+import com.sparta.project.cart.service.CartService;
+import com.sparta.project.cart.dto.request.ReqAddCartItemDto;
+import com.sparta.project.cart.dto.request.ReqUpdateCartItemQuantityDto;
+import com.sparta.project.cart.dto.response.ResCartDto;
+import com.sparta.project.cart.dto.response.ResUpdateCartItemQuantityDto;
+import com.sparta.project.global.infrastructure.presentation.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/cart")
+public class CartController {
+
+  private final CartService cartService;
+
+  // 장바구니 아이템 추가
+  @PostMapping("/items")
+  public ResponseEntity<ApiResponse<ResCartDto>> addItemToCart(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestBody @Valid ReqAddCartItemDto reqDto
+  ) {
+    ResCartDto cartDto = cartService.addCartItem(userDetails.getUserId(), reqDto);
+
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(ApiResponse.success(
+            HttpStatus.CREATED,
+            cartDto
+        ));
+  }
+
+
+  // 장바구니 보기
+  @GetMapping
+  public ResponseEntity<ApiResponse<ResCartDto>> getCart(
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    ResCartDto cartDto = cartService.getCart(userDetails.getUserId());
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(
+            HttpStatus.OK,
+            cartDto
+        ));
+  }
+
+
+  // 장바구니 수량 수정
+  @PatchMapping("/{cartItemId}")
+  public ResponseEntity<ApiResponse<ResUpdateCartItemQuantityDto>> updateCartItemQuantity(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable UUID cartItemId,
+      @RequestBody @Valid ReqUpdateCartItemQuantityDto reqDto
+  ) {
+
+    ResUpdateCartItemQuantityDto resDto = cartService.updateCartItemQuantity(
+        userDetails.getUserId(),
+        cartItemId,
+        reqDto.getQuantity()
+    );
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(
+            HttpStatus.OK,
+            resDto
+        ));
+  }
+
+
+  //장바구니 아이템 삭제
+  @DeleteMapping("/{cartItemId}")
+  public ResponseEntity<ApiResponse<Void>> deleteCartItem(
+      @AuthenticationPrincipal UserDetailsImpl userDetails,
+      @PathVariable UUID cartItemId
+  ) {
+    cartService.deleteCartItem(userDetails.getUserId(), cartItemId);
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(HttpStatus.OK));
+  }
+
+  //장바구니 아이템 전체 비우기
+  @DeleteMapping
+  public ResponseEntity<ApiResponse<Void>> clearCart(
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    cartService.clearCart(userDetails.getUserId());
+
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .body(ApiResponse.success(HttpStatus.OK));
+  }
+
+
+}
