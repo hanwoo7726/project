737c7aecb085322c232441d9ef2dfed883c4c7dd
kuyhxl <ozzing727@naver.com>
2026-07-15 11:15:26 +0900

    [Chore] main 병합 및 빌드 정리

--- src/main/java/com/sparta/project/repository/OrderStatusHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.project.repository;
-
-import com.sparta.project.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderStatusHistoryRepository extends JpaRepository<Order, String> {
-
-}
